31c1506d07f42ac16650c4c27f2d5b80a3769aa3
Manuel Alanis <manuelaag99@gmail.com>
2023-07-12 21:12:17 -0600

    functioning light/dark colorscheme

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import { Link } from 'expo-router';
+import { Link, SplashScreen } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useFonts } from "expo-font";
 
 import HomeScreen from './app/homescreen';
 
 export default function App() {
+  let colorScheme = useColorScheme();
+
+  let containerTheme
+  // containerTheme = styles.darkContainer
+  if (colorScheme === "dark") {
+    containerTheme = styles.darkContainer
+  } else {
+    containerTheme = styles.lightContainer
+  }
+
+  // const [fontsLoaded] = useFonts({
+  //     'Anton': require('../assets/fonts/Anton-Regular.ttf'),
+  // });
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={[styles.container, containerTheme]}>
         <View  style={styles.container}>
           {/* <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
           <Link href="/about">About</Link> */}
+          <StatusBar style="auto" />
           <HomeScreen />
         </View>
       </SafeAreaView>
@@ -24,12 +38,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
-    borderColor: "#aaa"
+    justifyContent: "center"
   },
-  text: {
-    color: "#000fff"
+  darkContainer: {
+    backgroundColor: "#1a1a1a"
+  },
+  darkThemeText: {
+    color: "#fff"
+  },
+  lightContainer: {
+    backgroundColor: "#fff"
+  },
+  lightThemeText: {
+    color: "#000"
   }
 });
--- app/homescreen.js
+++ app/homescreen.js
@@ -1,19 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+
 export default function HomeScreen () {
+
     const insets = useSafeAreaInsets();
     return (
-        <View style={styles.container}>
-            <Text style={{ fontSize: 30 }}>This is the Home Screen</Text>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", paddingTop: insets.top }}>
+            <View style={{ height: "50%", width: "100%", flex: 2 }}>
+                <View style={{ width: "50%" }}>
+                    <Text>Nice</Text>
+                </View>
+                <View style={{ width: "50%" }}>
+                    <Text>Thingsss</Text>
+                </View>
+            </View>
+            <View style={{ width: "100%", height: "50%" }}>
+                <Text style={{ color: "#ff0000", fontSize: 30, fontWeight: "bold" }}>MEXIA'S TACOS</Text>
+            </View>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
