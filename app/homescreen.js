7ea724fad5ede6bf5fba1d09eb806523e6cfe8ef
Manuel Alanis <manuelaag99@gmail.com>
2023-07-25 21:20:17 -0600

    add input and state

--- app/homescreen.js
+++ app/homescreen.js
@@ -1,23 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+const imageurl = { uri: "https://www.wcrf-uk.org/wp-content/uploads/2021/06/588595864r-LS.jpg" }
 
 export default function HomeScreen () {
+    const [order, setOrder] = useState()
+    const [listOfOrders, setListOfOrders] = useState([]);
+
+    function changeHandle (e) {
+        setOrder(e.target.value)
+        console.log(e.target.value)
+    }
 
     const insets = useSafeAreaInsets();
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", paddingTop: insets.top }}>
-            <View style={{ height: "50%", width: "100%", flex: 2 }}>
-                <View style={{ width: "50%" }}>
-                    <Text>Nice</Text>
-                </View>
-                <View style={{ width: "50%" }}>
-                    <Text>Thingsss</Text>
-                </View>
-            </View>
-            <View style={{ width: "100%", height: "50%" }}>
-                <Text style={{ color: "#ff0000", fontSize: 30, fontWeight: "bold" }}>MY APP</Text>
-            </View>
+        <View style={{ flex: 1, paddingTop: insets.top, alignSelf: "baseline" }}>
+            <ImageBackground source={imageurl} style={{ flex: 1, justifyContent: "center", width: "100%" }}>
+                <Text style={{ backgroundColor: "#000000a0", color: "#fff", fontSize: 40 }}>yeahhhhhhh</Text>
+                <input type="text" placeholder="Write the order here" onChange={changeHandle} />
+            </ImageBackground>
         </View>
     );
 }
