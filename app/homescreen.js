57d4ba606c0f8455f32297d0c25491ff828dd655
Manuel Alanis <manuelaag99@gmail.com>
2023-07-27 20:34:50 -0600

    add buttons to each order

--- app/homescreen.js
+++ app/homescreen.js
@@ -24,24 +24,30 @@ export default function HomeScreen () {
     return (
         <View style={[[ t.flex, t.justifyCenter, tw.wFull, tw.hScreen ], { paddingTop: insets.top}]}>
             <ImageBackground source={imageurl} style={[ t.flex, t.justifyCenter, tw.wFull, tw.hFull ]}>
-                <View style={[ t.flex, t.flexCol, t.justifyCenter, tw.h3_5, tw.wFull ]}>
+                <View style={[ t.flex, t.flexCol, t.justifyCenter, tw.h2_5, tw.wFull ]}>
                     <View style={[ t.flex, t.flexCol, tw.wAuto, t.justifyCenter, tw.h2_5, tw.pX2 ]}>
                         <View style={[ t.flex, t.flexCol, tw.wFull, t.justifyCenter, tw.h2_5 ]}>
                             <Text style={[ tw.bgBlack, t.textWhite, tw.text3xl, tw.pY2, t.textCenter ]}>Write down the order below</Text>
                             <View style={[ t.flex, t.flexRow ]}>
                                 <TextInput style={[ tw.w4_5, tw.bgWhite, tw.pX4 ]} onChangeText={changeHandle} value={order} />
-                                <TouchableHighlight style={[ tw.w1_5, tw.bgRed400, t.p3]} onPress={addButtonHandle} >
+                                <TouchableHighlight style={[ tw.w1_5, tw.bgYellow300, t.p3]} onPress={addButtonHandle} >
                                     <Text style={[tw.textCenter]}>ADD</Text>
                                 </TouchableHighlight>
                             </View>
                         </View>
                         <View style={[ t.flex, t.flexCol, tw.wAuto, tw.h3_5, t.overflowHidden, tw.bgWhite, tw.borderY, tw.borderSolid, t.justifyCenter, tw.flexGrow ]}>
-                            <View style={[ t.flex, tw.wFull, t.justifyCenter, tw.bgWhite, tw.borderY, tw.borderSolid, tw.mXAuto, tw.pY1 ]}>
+                            <View style={[ t.flex, tw.wFull, t.justifyCenter, tw.bgWhite, tw.borderY, tw.borderSolid, tw.mXAuto, tw.pY1, tw.mY0 ]}>
                                 <Text style={[ t.textCenter, tw.wFull ]}>List of orders:</Text>
                             </View>
                             {listOfOrders && listOfOrders.map((order, index) => {
-                                return <View key={index} style={[ t.flex, tw.wFull, t.justifyCenter, tw.bgWhite, tw.borderY, tw.borderSolid, tw.mXAuto, tw.pY1 ]}>
-                                    <Text style={[ tw.mY3, t.textCenter, tw.wFull, tw.hAuto ]}>{order}</Text>
+                                return <View key={index} style={[ t.flex, t.flexRow, tw.wFull, t.justifyCenter, tw.bgWhite, tw.borderY, tw.borderSolid, tw.mXAuto, tw.mY0 ]}>
+                                    <Text style={[ tw.mY3, t.textCenter, tw.w4_6, tw.hAuto, tw.hAuto ]}>{order}</Text>
+                                    <TouchableHighlight style={[ tw.w1_6, tw.bgBlue400, tw.hAuto ]} onPress={addButtonHandle} >
+                                        <Text style={[tw.textCenter]}>DONE</Text>
+                                    </TouchableHighlight>
+                                    <TouchableHighlight style={[ tw.w1_6, tw.bgRed400, tw.hAuto ]} onPress={addButtonHandle} >
+                                        <Text style={[tw.textCenter]}>DELETE</Text>
+                                    </TouchableHighlight>
                                 </View>
                             })}
                         </View>
