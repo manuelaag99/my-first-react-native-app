02fe595fab01360668e5cb8bff166660f2eb3197
Manuel Alanis <manuelaag99@gmail.com>
2023-07-27 20:26:07 -0600

    functioning basic list state

--- app/homescreen.js
+++ app/homescreen.js
@@ -22,27 +22,29 @@ export default function HomeScreen () {
 
     const insets = useSafeAreaInsets();
     return (
-        <View style={[[ t.flex, t.justifyCenter], { paddingTop: insets.top}]}>
+        <View style={[[ t.flex, t.justifyCenter, tw.wFull, tw.hScreen ], { paddingTop: insets.top}]}>
             <ImageBackground source={imageurl} style={[ t.flex, t.justifyCenter, tw.wFull, tw.hFull ]}>
-                <View style={[ t.flex, t.flexCol, t.justifyCenter, tw.h4_5 ]}>
-                    <View style={[ t.flex, t.flexCol, t.justifyCenter, tw.h2_5 ]}>
-                        <Text style={[ tw.bgBlack, t.textWhite, tw.text3xl ]}>Write down the order below</Text>
-                        <View style={[ t.flex, t.flexRow ]}>
-                            <TextInput style={[ tw.w4_5, tw.bgWhite, tw.pX4 ]} onChangeText={changeHandle} value={order} />
-                            <TouchableHighlight style={[ tw.w1_5, tw.bgRed400, t.p3]} onPress={addButtonHandle} >
-                                <Text style={[tw.textCenter]}>ADD</Text>
-                            </TouchableHighlight>
-                        </View>
-                    </View>
-                    <View style={[ t.flex, t.flexCol, tw.wFull, tw.h3_5, t.overflowHidden ]}>
-                        <View style={[ t.flex, tw.wFull, t.justifyCenter, tw.bgWhite, tw.border2, tw.borderSolid, tw.mAuto ]}>
-                            <Text style={[ tw.mY3, t.textCenter, tw.wFull, tw.hAuto ]}>List of orders:</Text>
-                        </View>
-                        {listOfOrders && listOfOrders.map((order, index) => {
-                            return <View key={index} style={[ t.flex, tw.wFull, t.justifyCenter, tw.bgWhite, tw.border2, tw.borderSolid, tw.mAuto ]}>
-                                <Text style={[ tw.mY3, t.textCenter, tw.wFull, tw.hAuto ]}>{order}</Text>
+                <View style={[ t.flex, t.flexCol, t.justifyCenter, tw.h3_5, tw.wFull ]}>
+                    <View style={[ t.flex, t.flexCol, tw.wAuto, t.justifyCenter, tw.h2_5, tw.pX2 ]}>
+                        <View style={[ t.flex, t.flexCol, tw.wFull, t.justifyCenter, tw.h2_5 ]}>
+                            <Text style={[ tw.bgBlack, t.textWhite, tw.text3xl, tw.pY2, t.textCenter ]}>Write down the order below</Text>
+                            <View style={[ t.flex, t.flexRow ]}>
+                                <TextInput style={[ tw.w4_5, tw.bgWhite, tw.pX4 ]} onChangeText={changeHandle} value={order} />
+                                <TouchableHighlight style={[ tw.w1_5, tw.bgRed400, t.p3]} onPress={addButtonHandle} >
+                                    <Text style={[tw.textCenter]}>ADD</Text>
+                                </TouchableHighlight>
                             </View>
-                        })}
+                        </View>
+                        <View style={[ t.flex, t.flexCol, tw.wAuto, tw.h3_5, t.overflowHidden, tw.bgWhite, tw.borderY, tw.borderSolid, t.justifyCenter, tw.flexGrow ]}>
+                            <View style={[ t.flex, tw.wFull, t.justifyCenter, tw.bgWhite, tw.borderY, tw.borderSolid, tw.mXAuto, tw.pY1 ]}>
+                                <Text style={[ t.textCenter, tw.wFull ]}>List of orders:</Text>
+                            </View>
+                            {listOfOrders && listOfOrders.map((order, index) => {
+                                return <View key={index} style={[ t.flex, tw.wFull, t.justifyCenter, tw.bgWhite, tw.borderY, tw.borderSolid, tw.mXAuto, tw.pY1 ]}>
+                                    <Text style={[ tw.mY3, t.textCenter, tw.wFull, tw.hAuto ]}>{order}</Text>
+                                </View>
+                            })}
+                        </View>
                     </View>
                 </View>
             </ImageBackground>
