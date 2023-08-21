ea88a5d090fd818f733116be9ec9aa4deadea6cb
Manuel Alanis <manuelaag99@gmail.com>
2023-08-20 18:27:22 -0600

    add auth bit to client page

--- app/supabase/client.js
+++ app/supabase/client.js
@@ -2,4 +2,10 @@ import { createClient } from '@supabase/supabase-js'
 import { API_KEY, API_URL } from "@env";
 
 // Use a custom domain as the supabase URL
-export const supabase = createClient(API_URL, API_KEY);
+export const supabase = createClient(API_URL, API_KEY, {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+      detectSessionInUrl: false
+    }
+});
