3c1c153fc523a07bdcd2c79cb7ffea47754cdaf4
Manuel Alanis <manuelaag99@gmail.com>
2023-08-30 20:19:56 -0600

    functioning fetch of employees and mapping to their user display names

--- app/Screens/RestaurantTeamScreen.js
+++ app/Screens/RestaurantTeamScreen.js
@@ -6,6 +6,8 @@ import { supabase } from "../supabase/client";
 export default function RestaurantTeamScreen ({ navigation, route }) {
     const [restaurantManager, setRestaurantManager] = useState();
     const [restaurantEmployeesArray, setRestaurantEmployeesArray] = useState();
+    const [restaurantEmployeesWithNamesArray, setRestaurantEmployeesWithNamesArray] = useState();
+    const [allUsers, setAllUsers] = useState();
 
     const { restaurant_id } = route.params;
 
@@ -14,26 +16,50 @@ export default function RestaurantTeamScreen ({ navigation, route }) {
             const { data, error } = await supabase.from("ALO-restaurants").select("*").eq("restaurant_id", restaurant_id);
             if (error) console.log(error);
             setRestaurantManager(data[0].creator_id);
-            // setRestaurantEmployeesArray();
+        } catch (err) {
+            console.log(err);
+        }
+        try {
+            const { data, error } = await supabase.from("ALO-employees").select("*").eq("restaurant_id", restaurant_id);
+            if (error) console.log(error);
+            setRestaurantEmployeesArray(data);
+        } catch (err) {
+            console.log(err);
+        }
+        try {
+            const { data, error } = await supabase.from("ALO-users-db").select("*");
+            if (error) console.log(error);
+            setAllUsers(data);
         } catch (err) {
             console.log(err);
         }
     }
 
-    const EMPLOYEESARRAY = [
-        { employee_name: "Luis Fdz", employee_id: "af921f7d-079b-43cc-a360-5a001e2345a3" },
-        { employee_name: "Jimmy McGill", employee_id: "89ebd3e4-e391-417a-a8e4-2d90b50eafda" },
-        { employee_name: "Chuck", employee_id: "c69313f0-d067-4d7c-af01-83167b055aa8" }
-    ]
+    // let restaurantEmployeesWithName;
+    function populateEmployeesArray() {
+        if (restaurantEmployeesArray) {
+            restaurantEmployeesArray.map((employee) => {
+                allUsers.map((user) => {
+                    if (user.user_id === employee.user_id) {
+                        return employee.employee_name = user.user_display_name
+                    }
+                })
+            })
+            setRestaurantEmployeesWithNamesArray(restaurantEmployeesArray);
+        }
+    }
 
     useEffect(() => {
         fetchRestaurantTeam();
-        setRestaurantEmployeesArray(EMPLOYEESARRAY)
     }, [])
 
-    if (!restaurantManager || !restaurantEmployeesArray) {
+    useEffect(() => {
+        populateEmployeesArray();
+    }, [allUsers])
+
+    if (!restaurantManager || !restaurantEmployeesWithNamesArray) {
         return <ActivityIndicator style={[ tw.mT10]} size="large" color="#000" />
-    } else if (restaurantManager && restaurantEmployeesArray) {
+    } else if (restaurantManager && restaurantEmployeesWithNamesArray) {
         return (
             <ScrollView>
                 <View style={[ t.flex, t.flexCol, tw.mY2, tw.pX6, tw.wFull ]}>
@@ -42,7 +68,7 @@ export default function RestaurantTeamScreen ({ navigation, route }) {
                         <Text style={[ tw.h6, t.textLeft, t.textGray600, t.italic ]}>Administrador</Text>
                     </View>}
 
-                    {restaurantEmployeesArray.map((employee, index) => {
+                    {restaurantEmployeesWithNamesArray.map((employee, index) => {
                         return (
                             <View key={index} style={[ t.flex, t.flexCol, tw.wFull, tw.pY2, t.borderT, t.borderGray400 ]}>
                                 <Text style={[ tw.h6, t.textLeft, t.textBlack, t.fontBold ]}>{employee.employee_name}</Text>
