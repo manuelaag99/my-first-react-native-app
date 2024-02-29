e3d97cc45d4db3adf61cd4f917c9752f13699e9d
Manuel Alanís <manuelaag99@gmail.com>
2024-02-29 13:22:26 -0600

    functional display of restaurants in deleting account screen

---
+++ app/Components/ListItem.js
@@ -0,0 +1,61 @@
+import { useContext, useEffect, useState } from "react";
+import { Text, TouchableHighlight, View } from "react-native";
+import { supabase } from "../supabase/client";
+import { t, tw } from "react-native-tailwindcss";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { AuthContext } from "../Context/AuthContext";
+
+export default function ListItem ({ buttonOne, buttonTwo, index, item, itemClassnames, itemElementClassnames, navigation }) {
+    const auth = useContext(AuthContext);
+    const [restaurantInfo, setRestaurantInfo] = useState();
+    const [restaurantAdmins, setRestaurantAdmins] = useState();
+    async function fetchRestaurantInfo () {
+        try {
+            const { data, error } = await supabase.from("ALO-restaurants").select("*").eq("restaurant_id", item.restaurant_id);
+            if (error) console.log(error);
+            setRestaurantInfo(data[0]);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    async function fetchRestaurantAdmins () {
+        try {
+            const { data, error } = await supabase.from("ALO-admins").select("user_id").eq("restaurant_id", item.restaurant_id);
+            if (error) console.log(error);
+            setRestaurantAdmins(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        fetchRestaurantAdmins();
+        fetchRestaurantInfo();
+    }, [])
+
+    if (restaurantAdmins && restaurantInfo) {
+        return (<View key={index} style={[[ tw.flex, tw.flexRow, tw.pY2, tw.wFull ], itemClassnames]}>
+            <View style={[[ tw.flex, tw.flexCol ], itemElementClassnames]}>
+                <Text style={[ tw.fontBold ]}>
+                    {restaurantInfo.restaurant_name}
+                </Text>
+                {restaurantAdmins && (restaurantAdmins.length = 1) && <Text style={[ t.textGray600 ]}>
+                    {restaurantAdmins.length} administrador, eres tú.
+                </Text>}
+                {restaurantAdmins && (restaurantAdmins.length = 1) && <Text style={[ t.textRed500 ]}>
+                    Si no nombras a otro administrador, toda la información de este restaurante y lo asociado a él se eliminará permanentemente.
+                </Text>}
+                {restaurantAdmins && (restaurantAdmins.length > 1) && <Text style={[ t.textGray600 ]}>
+                    {restaurantAdmins.length} administradores.
+                </Text>}
+                {restaurantAdmins && (restaurantAdmins.length > 1) && <Text style={[ t.textGreen500 ]}>
+                    Si eliminas tu cuenta, aún existirá información de este restaurante.
+                </Text>}
+            </View>
+            {buttonOne && <TouchableHighlight onPress={() => navigation.navigate("Restaurant", { restaurant_id: restaurantInfo.restaurant_id, user_id: auth.userId })} underlayColor="#ccc" style={[ tw.w1_5, tw.itemsCenter, tw.justifyCenter ]}>
+                <Text style={[ t.textBlack ]}>
+                    <Icon name="facebook" size={25} />
+                </Text>
+            </TouchableHighlight>}
+        </View>)
+    }
+}
--- app/Screens/DeleteUserAccountScreen.js
+++ app/Screens/DeleteUserAccountScreen.js
@@ -1,10 +1,26 @@
-import { View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import { supabase } from "../supabase/client";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Context/AuthContext";
+import ListItem from "../Components/ListItem";
+import { t, tw } from "react-native-tailwindcss";
 
-export default function DeleteUserAccountScreen () {
+export default function DeleteUserAccountScreen ({ navigation, route }) {
     const auth = useContext(AuthContext);
+    
+    const [allUserAdmins, setAllUserAdmins] = useState();
+    async function fetchAdmins () {
+        try {
+            const { data, error } = await supabase.from("ALO-admins").select("*").eq("user_id", auth.userId);
+            if (error) console.log(error);
+            setAllUserAdmins(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        fetchAdmins();
+    }, [])
 
     async function deleteUserInfoFromDataBase () {
         try {
@@ -27,9 +43,26 @@ export default function DeleteUserAccountScreen () {
         }
     }
 
-    return (
-        <View>
-
-        </View>
-    )
+    if (!allUserAdmins) {
+        return (
+            <View>
+                <ActivityIndicator style={[ tw.mT10]} size="large" color="#000" />
+            </View>
+        )
+    } else {
+        return (
+            <View style={[ tw.flex, tw.flexCol, tw.mY3, tw.pX5 ]}>
+                <View style={[ tw.mY6, tw.pX5, tw.flex, tw.justifyCenter, tw.itemsCenter ]}>
+                        <Text style={[ t.textCenter ]}>
+                            Mis restaurantes.
+                        </Text>
+                    </View>
+                {allUserAdmins && (allUserAdmins.length > 0) && allUserAdmins.map((admin, index) => {
+                    return (
+                        <ListItem buttonOne={true} item={admin} index={index} itemClassnames={null} itemElementClassnames={[tw.w4_5, tw.pR5]} navigation={navigation} />
+                    )
+                })}
+            </View>
+        )
+    }
 }
